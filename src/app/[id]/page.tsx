3a171eb0bb2fe2cd2aca75bb9b9d9a66d51fcd27
martin-bass <martin_reym@hotmail.com>
2024-03-02 18:24:15 -0300

    first commit

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,51 @@
+import api from "@/api";
+import Link from "next/link";
+
+export async function generateMetadata({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const restaurant = await api.fetch(id);
+
+  return {
+    title: `${restaurant.name} - Restaurancy`,
+    description: restaurant.description,
+  };
+}
+
+export async function generateStaticParams() {
+  const restaurants = await api.list();
+
+  return restaurants.map((restaurant) => ({
+    id: restaurant.id,
+  }));
+}
+
+export default async function RestaurantPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const restaurant = await api.fetch(id);
+
+  return (
+    <article key={restaurant.id}>
+      <Link href={"/"}>Volver...</Link>
+      <img
+        alt={restaurant.name}
+        className="mb-3 h-[300px] w-full object-cover"
+        src={restaurant.image}
+      />
+      <h2 className="inline-flex gap-2 text-lg font-bold">
+        <span>{restaurant.name}</span>
+        <small className="inline-flex gap-1">
+          <span>⭐</span>
+          <span>{restaurant.score}</span>
+          <span className="font-normal opacity-75">({restaurant.ratings})</span>
+        </small>
+      </h2>
+      <p className="opacity-90">{restaurant.description}</p>
+    </article>
+  );
+}
